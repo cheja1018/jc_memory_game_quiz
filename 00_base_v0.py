b960caf73119c5d28fe0c99667f0ff8ba1f1cd9f
cheja1018 <72240876+cheja1018@users.noreply.github.com>
2022-08-05 14:58:04 +1200

    skeleton of base code

---
+++ 00_base_v0.py
@@ -0,0 +1,15 @@
+"""
+base component for my memory quiz game
+V0 - skeleton setup
+author - Jasmine Chen
+2022
+"""
+
+# import libraries below this line **************************
+
+
+# all functions below this line **************************
+
+
+# main routine below this line **************************
+print("Hello")
